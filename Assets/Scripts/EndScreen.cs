636e1a27f4640e6b2c5a48849b7e8febdaf2b6ee
Filip <filip4125@gmail.com>
2023-02-01 16:39:36 +0100

    Urađeno platno za završnu scenu

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndScreen : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI finalScoreText;
+
+
+    public void ShowFinalScore()
+    {
+        finalScoreText.text = "Congratulations!\nYou scored " + ScoreKeeper.instance.CalculateScore() + "%";
+    }
+
+}
